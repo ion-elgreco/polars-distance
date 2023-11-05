5ca2a8c60f30b1fe0ee87806c4e1629ce95f7afd
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2023-11-05 02:33:51 +0100

    disable pdist on lists/arrays

--- polars_distance/polars_distance/src/expressions.rs
+++ polars_distance/polars_distance/src/expressions.rs
@@ -1,10 +1,8 @@
 use polars::prelude::*;
 use pyo3_polars::derive::polars_expr;
 use distances::strings::{hamming, levenshtein};
-use distances::simd::euclidean_f64;
-// use distances::vectors::{euclidean};
-
-use pyo3_polars::export::polars_core::with_match_physical_float_polars_type;
+// use distances::simd::euclidean_f64;
+// use pyo3_polars::export::polars_core::with_match_physical_float_polars_type;
 
 
 fn hamming_distance_string(x: &str, y: &str) -> u32 {
@@ -15,29 +13,29 @@ fn levenshtein_distance_string(x: &str, y: &str) -> u32 {
     levenshtein(x, y)
 }
 
-fn euclidean_vector_simd(a: &ListChunked, b: &ListChunked) -> PolarsResult<Float64Chunked> {
-    polars_ensure!(
-        a.inner_dtype() == b.inner_dtype(),
-        ComputeError: "inner data types don't match"
-    );
-    polars_ensure!(
-        a.inner_dtype().is_float(),
-        ComputeError: "inner data types must be float"
-    );
+// fn euclidean_vector_simd(a: &ListChunked, b: &ListChunked) -> PolarsResult<Float64Chunked> {
+//     polars_ensure!(
+//         a.inner_dtype() == b.inner_dtype(),
+//         ComputeError: "inner data types don't match"
+//     );
+//     polars_ensure!(
+//         a.inner_dtype().is_float(),
+//         ComputeError: "inner data types must be float"
+//     );
 
-    Ok(with_match_physical_float_polars_type!(a.inner_dtype(), |$T| {
-    polars::prelude::arity::binary_elementwise(a, b, |a, b| {
-        match (a, b) {
-            (Some(a), Some(b)) => {
-                let a = a.as_any().downcast_ref::<Vec<f64>>().unwrap();
-                let b = b.as_any().downcast_ref::<Vec<f64>>().unwrap();
-                Some(euclidean_f64(a, b))
-            },
-            _ => None
-        }
-    })
-    }))
-}
+//     Ok(with_match_physical_float_polars_type!(a.inner_dtype(), |$T| {
+//     polars::prelude::arity::binary_elementwise(a, b, |a, b| {
+//         match (a, b) {
+//             (Some(a), Some(b)) => {
+//                 let a = a.as_any().downcast_ref::<Vec<f64>>().unwrap();
+//                 let b = b.as_any().downcast_ref::<Vec<f64>>().unwrap();
+//                 Some(euclidean_f64(a, b))
+//             },
+//             _ => None
+//         }
+//     })
+//     }))
+// }
 
 #[polars_expr(output_type=UInt32)]
 fn hamming_string(inputs: &[Series]) -> PolarsResult<Series> {
@@ -66,13 +64,13 @@ fn levenshtein_string(inputs: &[Series]) -> PolarsResult<Series> {
     Ok(out.into_series())
 }
 
-#[polars_expr(output_type=Float64)]
-fn euclidean_accelerated(inputs: &[Series]) -> PolarsResult<Series> {
-    // if inputs[0].dtype() != &DataType::List || inputs[1].dtype() != &DataType::List {
-    //     polars_bail!(InvalidOperation: "");
-    // }
-    let x = inputs[0].list()?;
-    let y = inputs[1].list()?;
+// #[polars_expr(output_type=Float64)]
+// fn euclidean_accelerated(inputs: &[Series]) -> PolarsResult<Series> {
+//     // if inputs[0].dtype() != &DataType::List || inputs[1].dtype() != &DataType::List {
+//     //     polars_bail!(InvalidOperation: "");
+//     // }
+//     let x = inputs[0].list()?;
+//     let y = inputs[1].list()?;
     
-    euclidean_vector_simd(x, y).map(|ca| ca.into_series())
-}
+//     euclidean_vector_simd(x, y).map(|ca| ca.into_series())
+// }

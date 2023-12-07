3242e18267998fbab9ce4bd4fa106f5e71389cb5
ion-elgreco <15728914+ion-elgreco@users.noreply.github.com>
2023-12-07 11:58:04 +0100

    wip

--- polars_distance/polars_distance/src/expressions.rs
+++ polars_distance/polars_distance/src/expressions.rs
@@ -1,5 +1,7 @@
 use distances::strings::{hamming, levenshtein};
-use polars::prelude::*;
+use polars::prelude::{*};
+use polars::prelude::arity::{binary_elementwise, try_binary_elementwise};
+use distances::simd::euclidean_f64;
 use pyo3_polars::derive::polars_expr;
 
 fn hamming_distance_string(x: &str, y: &str) -> u32 {
@@ -8,6 +10,39 @@ fn hamming_distance_string(x: &str, y: &str) -> u32 {
 
 fn levenshtein_distance_string(x: &str, y: &str) -> u32 {
     levenshtein(x, y)
+}
+
+
+fn euclidean_vector_simd(a: &ChunkedArray<FixedSizeListType>, b: &ChunkedArray<FixedSizeListType>) -> PolarsResult<Float64Chunked> {
+    polars_ensure!(
+        a.inner_dtype() == b.inner_dtype(),
+        ComputeError: "inner data types don't match"
+    );
+    polars_ensure!(
+        a.inner_dtype().is_numeric(),
+        ComputeError: "inner data types must be numeric"
+    );
+
+    try_binary_elementwise(a, b, |a, b| {
+        match (a, b) {
+            (Some(a), Some(b)) => {
+                // Ok(Some(1.0))
+                dbg!(&a);
+                dbg!(&b);
+                let a = a.as_any().downcast_ref::<Vec<_>>();
+                let b = b.as_any().downcast_ref::<Vec<_>>();
+                dbg!(&a);
+                dbg!(&b);
+                match (a, b) {
+                    (Some(a), Some(b)) => {
+                        Ok(Some(euclidean_f64(a, b)))
+                    }
+                    _ => Ok(None)
+                }
+            },
+            _ => Ok(None)
+        }
+    })
 }
 
 #[polars_expr(output_type=UInt32)]
@@ -32,4 +67,18 @@ fn levenshtein_string(inputs: &[Series]) -> PolarsResult<Series> {
 
     let out: UInt32Chunked = arity::binary_elementwise_values(x, y, levenshtein_distance_string);
     Ok(out.into_series())
+}
+
+#[polars_expr(output_type=Float64)]
+fn euclidean_accelerated(inputs: &[Series]) -> PolarsResult<Series> {
+    let x: &ChunkedArray<FixedSizeListType> = inputs[0].array()?;
+    let y: &ChunkedArray<FixedSizeListType> = inputs[1].array()?;
+
+    if x.width() != y.width() {
+        polars_bail!(InvalidOperation:
+            "The dimensions of each array are not the same. 
+                `{}` width: {}, 
+                `{}` width: {}", inputs[0].name(), x.width(), inputs[1].name(), y.width());
+    }
+    euclidean_vector_simd(x, y).map(|ca| ca.into_series())
 }

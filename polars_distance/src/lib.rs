014d33d55342f6cfc558ebb8a242b2c1f3171c3d
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2024-11-01 12:49:38 +0100

    chore: pyo3 deprecation

--- polars_distance/src/lib.rs
+++ polars_distance/src/lib.rs
@@ -4,7 +4,7 @@ mod list;
 mod other_dist;
 mod string;
 use pyo3::types::PyModule;
-use pyo3::{pymodule, PyResult, Python};
+use pyo3::{pymodule, Bound, PyResult, Python};
 
 #[cfg(target_os = "linux")]
 use jemallocator::Jemalloc;
@@ -14,7 +14,7 @@ use jemallocator::Jemalloc;
 static ALLOC: Jemalloc = Jemalloc;
 
 #[pymodule]
-fn _internal(_py: Python, m: &PyModule) -> PyResult<()> {
+fn _internal(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }

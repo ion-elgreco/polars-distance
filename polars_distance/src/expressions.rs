2f7795316984006517c5cbae8027618ab25d6067
Louis Maddox <louismmx@gmail.com>
2025-03-06 14:38:46 +0000

    revert: put the series conversion in the closure, avoid generic return types

--- polars_distance/src/expressions.rs
+++ polars_distance/src/expressions.rs
@@ -378,23 +378,23 @@ where
     match (x.inner_dtype(), y.inner_dtype(), &output_type) {
         // Both Float32 -> Float32 output
         (DataType::Float32, DataType::Float32, DataType::Float32) => {
-            f32_impl(x, y).map(|ca| ca.into_series())
+            f32_impl(x, y)
         },
         // Both Float64 -> Float64 output
         (DataType::Float64, DataType::Float64, DataType::Float64) => {
-            f64_impl(x, y).map(|ca| ca.into_series())
+            f64_impl(x, y)
         },
         // Float32 and Float64 -> cast Float32 to Float64, Float64 output
         (DataType::Float32, DataType::Float64, DataType::Float64) => {
             // Cast x to Float64
             let x_f64 = x.cast(&DataType::Array(Box::new(DataType::Float64), x.width()))?;
-            f64_impl(x_f64.array()?, y).map(|ca| ca.into_series())
+            f64_impl(x_f64.array()?, y)
         },
         // Float64 and Float32 -> cast Float32 to Float64, Float64 output
         (DataType::Float64, DataType::Float32, DataType::Float64) => {
             // Cast y to Float64
             let y_f64 = y.cast(&DataType::Array(Box::new(DataType::Float64), y.width()))?;
-            f64_impl(x, y_f64.array()?).map(|ca| ca.into_series())
+            f64_impl(x, y_f64.array()?)
         },
         // This should be unreachable since we've filtered the types above
         _ => unreachable!()
@@ -412,8 +412,8 @@ fn euclidean_arr(inputs: &[Series]) -> PolarsResult<Series> {
         x, 
         y, 
         "euclidean",
-        |x_f32, y_f32| crate::array::euclidean_dist::<Float32Type>(x_f32, y_f32),
-        |x_f64, y_f64| crate::array::euclidean_dist::<Float64Type>(x_f64, y_f64)
+        |x_f32, y_f32| crate::array::euclidean_dist::<Float32Type>(x_f32, y_f32).map(|ca| ca.into_series()),
+        |x_f64, y_f64| crate::array::euclidean_dist::<Float64Type>(x_f64, y_f64).map(|ca| ca.into_series())
     )
 }
 
